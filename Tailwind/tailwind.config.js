21a4a2f38b77d3801780b0eab2dc3f6ac093a7e6
Pierre Nangi <71530777+ynwbape@users.noreply.github.com>
2022-12-22 20:38:00 +0100

    Grosse MàJ du projet
    les features suivantes :
     - Carte google maps,
     - Fonds d'images en bg
     - Widgets Spotify
     - Youtube intégrée
     - Track by Track Eyes
     - Accueil, Header, Footer, Album & Tracklist terminé
     - Image Slider

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../website/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
+
---
+++ website/Scripts/index.js
@@ -0,0 +1,17 @@
+// Track by Track eyes
+const iframeElement = document.querySelector('#tbyt-frame');
+
+let playFrameFunc = frameEl =>  {
+    iframeElement.setAttribute('src', frameEl.getAttribute('data-src'));
+}
+
+let $isFrameOpen = false; 
+
+$('div.iframeDiv').hide();
+
+$('button.open-frame-btn').click(function (e) { 
+    e.preventDefault();
+    $('div.iframeDiv').show();
+    console.log(e);
+});
+
